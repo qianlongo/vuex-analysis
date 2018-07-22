aff4c673aacd126a32d0f2fbe1e924c10608f12d
qianlongo <1162633511@qq.com>
2018-07-22 12:35:03 +0800

    update: 工具辅助方法解析

--- src/util.js
+++ src/util.js
@@ -49,18 +49,19 @@ export function deepCopy (obj, cache = []) {
 /**
  * forEach for object
  */
+// 为遍历对象而封装的函数
 export function forEachValue (obj, fn) {
   Object.keys(obj).forEach(key => fn(obj[key], key))
 }
-
+// 判断obj是否是对象, 我们一般不是通过Object.prototype.toString.call(obj) === '[object Object]'来判断吗
 export function isObject (obj) {
   return obj !== null && typeof obj === 'object'
 }
-
+// 简单判断val是不是Promise
 export function isPromise (val) {
   return val && typeof val.then === 'function'
 }
-
+// 简单的断言函数
 export function assert (condition, msg) {
   if (!condition) throw new Error(`[vuex] ${msg}`)
 }
