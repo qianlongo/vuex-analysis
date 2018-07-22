e0173fad3706a0e50ff5b84c2404c2ba521a86b5
qianlongo <1162633511@qq.com>
2018-07-22 15:21:30 +0800

    update: mapState解析

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,29 @@
+
+/*
+// 在单独构建的版本中辅助函数为 Vuex.mapState
+import { mapState } from 'vuex'
+
+export default {
+  // ...
+  computed: mapState({
+    // 箭头函数可使代码更简练
+    count: state => state.count,
+
+    // 传字符串参数 'count' 等同于 `state => state.count`
+    countAlias: 'count',
+
+    // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+    countPlusLocalState (state) {
+      return state.count + this.localCount
+    }
+  })
+}
+
+computed: mapState([
+  // 映射 this.count 为 store.state.count
+  'count'
+])
+*/
 export function mapState (states) {
   const res = {}
   normalizeMap(states).forEach(({ key, val }) => {
